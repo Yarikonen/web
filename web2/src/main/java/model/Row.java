7d64c311c1677abe92a7470860756c26c5fa4ff4
Ярослав Рудык <67190436+Yarikonen@users.noreply.github.com>
2022-10-16 18:01:20 +0300

    changed time view

--- web2/src/main/java/model/Row.java
+++ web2/src/main/java/model/Row.java
@@ -3,6 +3,7 @@ package model;
 import java.io.Serializable;
 import java.time.LocalTime;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
 public class Row implements Serializable {
     private double x;
@@ -14,23 +15,23 @@ public class Row implements Serializable {
         this.x=x;
         this.y=y;
         this.r = r;
-        this.birthTime=LocalTime.now(zone);
+        this.birthTime=LocalTime.now(zone).truncatedTo(ChronoUnit.SECONDS);
         this.exTime=System.nanoTime();
     }
 
     public void isHit() {
         if (x>=0 &&y>=0 && y<=r && x<=r/2) booling = true;
         if (x>0 && y<=0 && x*x+y*y<=r*r/4) booling = true;
-        if (x<0 && y<=0 && y>=x-r) booling = true;
+        if (x<0 && y<=0 && y>=-x-r) booling = true;
         exTime = -exTime + System.nanoTime();
 
     }
     @Override
     public String toString() {
         return ("<tr><td>" + booling + "</td><td>" + exTime + "</td><td>" + birthTime +  "</td>"
-                + "<td>" + x + "</td>"
-                + "<td>" + y + "</td>"
-                + "<td>" + r +  "</td></tr>");
+                + "<td name=xT>" + Math.round(x*1000.0)/1000.0 + "</td>"
+                + "<td name=yT>" + Math.round(y*1000.0)/1000.0 + "</td>"
+                + "<td name=rT>" + r +  "</td></tr>");
     }
 
 
