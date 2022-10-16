d9fb54c52d34b96377b638a392c7ac855f226cbc
Ярослав Рудык <67190436+Yarikonen@users.noreply.github.com>
2022-10-16 17:55:28 +0300

    header

--- web2/src/main/webapp/svgConnector.js
+++ web2/src/main/webapp/svgConnector.js
@@ -1,3 +1,17 @@
+var svg = document.querySelector('svg');
+var height = svg.getAttribute("height");
+var width =  svg.getAttribute("width");
+function graphPoint(evt){
+    let pt = svg.createSVGPoint();
+    pt.x = evt.clientX; pt.y = evt.clientY;
+    return pt.matrixTransform(svg.getScreenCTM().inverse());
+}
+function screenPoint(x,y,r){
+    let pt = svg.createSVGPoint();
+    pt.x = x*248/r + width/2
+    pt.y = -y*248/r + height/2
+    return(pt)
+}
 
 document.getElementById('image1').addEventListener('click', function (event) {
     var get_R;
