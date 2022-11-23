33276ed58be2b410b45c44ee5d50b3d70e64ddea
Ярослав Рудык <67190436+Yarikonen@users.noreply.github.com>
2022-11-24 00:25:54 +0300

    LETSGOOOOOOo web3 starts here

---
+++ web3/src/main/java/User.java
@@ -0,0 +1,19 @@
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    String name="aboba";
+    public User(){
+
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+}
