6ec057a8a4880ae01bce660a23f7a18b8c900404
Ярослав Рудык <67190436+Yarikonen@users.noreply.github.com>
2023-01-19 19:51:19 +0300

    minor changes

--- web4/src/main/java/com/yarikonen/web44/Web4Application.java
+++ web4/src/main/java/com/yarikonen/web44/Web4Application.java
@@ -3,11 +3,12 @@ package com.yarikonen.web44;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class Web44Application {
+public class Web4Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Web44Application.class, args);
+        SpringApplication.run(Web4Application.class, args);
     }
 
 }
