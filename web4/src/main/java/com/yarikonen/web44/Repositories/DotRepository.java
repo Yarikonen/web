6d69fd5c7272dc7c800d1916c1603f7b3bf7536c
Ярослав Рудык <67190436+Yarikonen@users.noreply.github.com>
2023-01-19 19:50:26 +0300

    repository added

---
+++ web4/src/main/java/com/yarikonen/web44/Repositories/DotRepository.java
@@ -0,0 +1,7 @@
+package com.yarikonen.web44.Repositories;
+
+import com.yarikonen.web44.Data.Dot;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface DotRepository extends JpaRepository<Dot,Long> {
+}
