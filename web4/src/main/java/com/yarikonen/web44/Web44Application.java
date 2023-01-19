ab7e9a932869af8dec22c46165a0a4ddf5a20021
Ярослав Рудык <67190436+Yarikonen@users.noreply.github.com>
2023-01-19 18:26:57 +0300

    4 init commit

---
+++ web4/src/main/java/com/yarikonen/web44/Web44Application.java
@@ -0,0 +1,13 @@
+package com.yarikonen.web44;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web44Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web44Application.class, args);
+    }
+
+}
---
+++ web4/src/test/java/com/yarikonen/web44/Web44ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yarikonen.web44;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web44ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
